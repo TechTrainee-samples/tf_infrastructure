53686db0d890e1d4d15720efcbf031d8e63d315c
Tech Student <ant.v2@yahoo.com>
2026-04-30 17:05:28 -0400

    Updated basic configuration

---
+++ tf_basics/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  access_key = "AWS_ACCESS_KEY_ID"
+  secret_key = "AWS_SECRET_ACCESS_KEY"
+  region     = "us-east-1"
+}
+
+resource "aws_instance" "tfDemo" {
+  ami = "ami-099b5eec69f378993"
+  instance_type = "t3.medium"
+
+  tags = {
+    Name = "tfInterview"
+  }
+}
